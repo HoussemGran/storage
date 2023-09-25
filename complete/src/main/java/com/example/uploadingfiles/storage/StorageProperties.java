36c913d40b826bec6e82662862981eb70ce9c786
houssem.gran <houssem.gran@proxym-it.com>
2023-09-25 23:29:48 +0100

    change location

--- complete/src/main/java/com/example/uploadingfiles/storage/StorageProperties.java
+++ complete/src/main/java/com/example/uploadingfiles/storage/StorageProperties.java
@@ -8,7 +8,7 @@ public class StorageProperties {
 	/**
 	 * Folder location for storing files
 	 */
-	private String location = "";
+	private String location = "/home";
 
 	public String getLocation() {
 		return location;
